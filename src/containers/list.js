6477465c6cd4bafef79f510630bb932b3172619f
Anu Olajide <102215210+pasta022@users.noreply.github.com>
2023-01-16 20:12:15 +0100

    feat: created a card component to render the movies and in turn rendered the card compomemt with a list container

---
+++ src/containers/list.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Card from '../components/card';
+
+class List extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true,
+        };
+    }
+    
+    async componentDidMount() {
+        const movies = await fetch('../../assets/data.json');
+        const moviesJSON = await movies.json();
+        if (moviesJSON) {
+            this.setState({
+                data: moviesJSON, loading: false,
+            });
+        }
+    }
+
+    render() { 
+        const { data, loading } = this.state;
+        if (loading) {
+            return <div>Loading...</div>
+            }
+        return data.map(movie => <Card key={movie.id} movie={movie} />); 
+    }
+}
+ 
+export default List;
